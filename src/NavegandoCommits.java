4bafa1b545444d0e76c41ea5a05b050e00b15c98
Andre Soares <andresoares37@gmail.com>
2022-11-16 21:30:17 -0300

    Incluindo exercicio de média

--- src/NavegandoCommits.java
+++ src/NavegandoCommits.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
 
 public class NavegandoCommits {
 
 	public static void main(String[] args) {
-		/*Cadê o conteúdo que estava aqui?*/
-		
-		System.out.println("Nossa branch agora termina aqui");
-
+		Scanner leitor = new Scanner(System.in);
+		System.out.println(
+				"Para calcularmos a média das notas dos alunos é preciso saber, primeiramente, quantos alunos estão na turma:");
+		int qtdeAlunos = leitor.nextInt();
+		int i = 0;
+		double mediaAlunos = 0;
+		while (i < qtdeAlunos) {
+			System.out.println("Digite a nota do " + (i + 1) + "º aluno:");
+			double notaAluno = leitor.nextDouble();
+			mediaAlunos = mediaAlunos + notaAluno;
+			i++;
+		}
+		mediaAlunos = mediaAlunos / qtdeAlunos;
+		System.out.println("A média de notas dos alunos dessa turma é " + mediaAlunos);
+		leitor.close();
 	}
 
 }
